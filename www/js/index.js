917a06416b15ca32ada88cb07c82b82107c6d31d
makba8 <arthur.cariou88@gmail.com>
2023-11-15 22:20:20 +0100

    Refont en monopage

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,78 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+this.pageInscrCo = document.getElementById("choixInscrCo");
+this.btnInscriptionInscrCo = document.getElementById("inscriptionBtn");
+this.btnConnexionInscrCo = document.getElementById("connexionBtn");
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener("deviceready", onDeviceReady, false);
+this.pageInscription = document.getElementById("inscription");
+this.revenirBtnInscr = document.getElementById("revenirBtnInscr");
+this.validerBtnInscr = document.getElementById("validerBtnInscr");
 
-function onDeviceReady() {
-  // Cordova is now initialized. Have fun!
+this.pageConnexion = document.getElementById("connexion");
+this.revenirBtnCo = document.getElementById("revenirBtnCo");
+this.validerBtnCo = document.getElementById("validerBtnCo");
 
-  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
-  document.getElementById("deviceready").classList.add("ready");
-}
+this.pageAccueil = document.getElementById("accueil");
+let timer = document.getElementById("timer");
+let startBtn = document.getElementById("startBtn");
+
+////////////////////////////////////////////////////////////////
+// page choix inscription connexion
+////////////////////////////////////////////////////////////////
+btnInscriptionInscrCo.addEventListener("click", (event) => {
+  pageInscrCo.style.display = "none";
+  pageInscription.style.display = "flex";
+});
+btnConnexionInscrCo.addEventListener("click", (event) => {
+  pageInscrCo.style.display = "none";
+  pageConnexion.style.display = "flex";
+});
+
+////////////////////////////////////////////////////////////////
+//page d'inscription
+////////////////////////////////////////////////////////////////
+revenirBtnInscr.addEventListener("click", (event) => {
+  pageInscrCo.style.display = "flex";
+  pageInscription.style.display = "none";
+});
+validerBtnInscr.addEventListener("click", (event) => {
+  // enregistrer les infos dans la BDD
+
+  //si infos vide, ne pas faire disparaitre l'écran
+  pageInscription.style.display = "none";
+  pageAccueil.style.display = "flex";
+});
+
+////////////////////////////////////////////////////////////////
+//page de connexion
+////////////////////////////////////////////////////////////////
+revenirBtnCo.addEventListener("click", (event) => {
+  pageInscrCo.style.display = "flex";
+  pageConnexion.style.display = "none";
+});
+validerBtnCo.addEventListener("click", (event) => {
+  // vérifier les infos dans la BDD
+  //si mauvaise ne pas faire disparaitre l'écran
+  pageConnexion.style.display = "none";
+});
+
+////////////////////////////////////////////////////////////////
+//page d'accueil
+////////////////////////////////////////////////////////////////
+
+// ajout de l'évènement click du bouton pour lancer le timer
+startBtn.addEventListener("click", function () {
+  let decompte = 30; // depart à 30 secondes
+
+  // Mise à jour du décompte
+  let timerInterval = setInterval(function () {
+    decompte--;
+    timer.textContent = decompte;
+    // Si le compte à rebours atteint zéro, arrêtez le minuteur
+    // A TERMINER : ici, on pourra gérer si un autre joueur a été trouvé ou pas dans le if
+    // si décompte = 0 ET que personne n'a été trouvé, afficher un message,
+    // sinon si quelqu'un a été trouvé avant la fin, on arrête le timer
+    if (decompte === 0) {
+      clearInterval(timerInterval); // faire cesser le décompte
+      alert("Aucun joueur trouvé pour vous affrontez...");
+    }
+  }, 1000); // Intervale de 1000 milisecondes soit 1 seconde
+});
